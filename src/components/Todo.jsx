84b115bc143059ba17e5d94c93979af52ca1eb58
Pavan Kumar <pavanmaddala16@gmail.com>
2023-03-11 11:21:57 +0530

    Modified Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,11 +2,9 @@ function Todo({ todo, onChange, onDelete, onUpdate }) {
   return (
     <div>
       <input type="checkbox" onChange={() => onChange(todo)} />
-      {!todo.completed ? (
-        <span>{todo.text}</span>
-      ) : (
-        <span style={{ textDecoration: "line-through" }}>{todo.text}</span>
-      )}
+      <span style={{ textDecoration: todo.completed ? "line-through" : "" }}>
+        {todo.text}
+      </span>
       <button onClick={() => onUpdate(todo)}>Edit</button>
       <button onClick={() => onDelete(todo.id)}>Delete</button>
     </div>
